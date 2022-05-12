38a2d502c095d5a49095e15e1509949129036150
saifmanihar <96367331+Saifmanihar@users.noreply.github.com>
2022-05-12 13:27:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function add(){
+    let a1=document.getElementsByClassName("number1")
+    let a2=document.getElementsByClassName("number2")
+    
+    number1=parseFloat(a1[0].value)
+    number2=parseFloat(a2[0].value)
+
+    sum =number1+number2;
+
+    let result=document.getElementById("result");
+    result.innerHTML=sum;
+}
+function minus(){
+    let a1=document.getElementsByClassName("number1")
+    let a2=document.getElementsByClassName("number2")
+    
+    number1=parseFloat(a1[0].value)
+    number2=parseFloat(a2[0].value)
+
+    sum =number1-number2;
+
+    let result=document.getElementById("result");
+    result.innerHTML=sum;
+}
+function multipy(){
+    let a1=document.getElementsByClassName("number1")
+    let a2=document.getElementsByClassName("number2")
+    
+    number1=parseFloat(a1[0].value)
+    number2=parseFloat(a2[0].value)
+
+    sum =number1*number2;
+
+    let result=document.getElementById("result");
+    result.innerHTML=sum;
+}
+function divide(){
+    let a1=document.getElementsByClassName("number1")
+    let a2=document.getElementsByClassName("number2")
+    
+    number1=parseFloat(a1[0].value)
+    number2=parseFloat(a2[0].value)
+
+    sum =number1/number2;
+
+    let result=document.getElementById("result");
+    result.innerHTML=sum;
+}
